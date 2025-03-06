913138ab89b8bfdd11695ffd10560e9a43234026
pboon09 <pboonchanachai@gmail.com>
2025-03-06 10:45:45 +0700

    Create params.m

---
+++ Plotter Multibody Simulation/params.m
@@ -0,0 +1,29 @@
+g = 9.80665;
+L = 0.3;
+
+b_revolute = 0;
+b_prismatic = 0;
+
+cover_flage_Ixx = 425.83e-3;
+cover_flage_Iyy = 313020.84e-9;
+cover_flage_Izz = 171937.07e-9;
+cover_flage_mass = 477713.12e-9;
+
+slide_rail_Ixx = 49757667.19e-9;
+slide_rail_Iyy = 1225718.53e-9;
+slide_rail_Izz = 50950095.13e-9;
+slide_rail_mass = 1893.96e-3;
+
+plotter_Ixx = 1020611.12e-9;
+plotter_Iyy = 1146620.14e-9;
+plotter_Izz = 456002.44e-9;
+plotter_mass = 500e-3;
+
+Ke = 0.5265;
+Km = 0.5265 / 2.0;
+L = 183.67e-6;
+R = 0.6367;
+J = 0.013369679361673;
+B = 0.019688440522932;
+
+V_max = 12.0;
