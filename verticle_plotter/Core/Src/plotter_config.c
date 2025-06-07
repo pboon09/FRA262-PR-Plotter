71594d478632691644a5938455c2f035ea50474c
pboon09 <pboonchanachai@gmail.com>
2025-06-08 02:23:29 +0700

    2.23

--- verticle_plotter/Core/Src/plotter_config.c
+++ verticle_plotter/Core/Src/plotter_config.c
@@ -157,10 +157,10 @@ void plotter_begin() {
 
 	PID_CONTROLLER_Init(&prismatic_position_pid, 85, 2e-1, 200,
 			ZGX45RGG_400RPM_Constant.sd_max);
-	PID_CONTROLLER_Init(&prismatic_velocity_pid, 120, 1e-5, 0,
+	PID_CONTROLLER_Init(&prismatic_velocity_pid, 120, 2e-5, 0,
 			ZGX45RGG_400RPM_Constant.U_max);
 
-	PID_CONTROLLER_Init(&revolute_position_pid, 250, 0.001, 700,
+	PID_CONTROLLER_Init(&revolute_position_pid, 250, 0.005, 700,
 			ZGX45RGG_150RPM_Constant.qd_max);
 	PID_CONTROLLER_Init(&revolute_velocity_pid, 2280, 25, 3000, //2280->2275 (P), 25->30 (I), 3000 -> 2996 (D)
 			ZGX45RGG_150RPM_Constant.U_max);
